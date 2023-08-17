18406e3ed5445aac81a98e470c14b091df2e1153
aniksikder13 <aniksikder9501@gmail.com>
2023-08-17 10:53:43 +0600

    first part of form ui

--- components/forms/form.js
+++ components/forms/form.js
@@ -7,16 +7,29 @@ import Button from '../ui/button'
 export default function Form() {
     const [nextStep, setNextStep]= useState(false)
 
+    const onFormHandler = (e) => {
+        e.preventDefault()
+        setNextStep(true)
+    }
+
   return (
-    <form className='mt-5 max-w-[800px] bg-white m-auto border px-3 py-4 rounded-md'>
+    <form className='mt-5 max-w-[800px] bg-white m-auto border px-3 py-4 rounded-md' onSubmit={onFormHandler}>
         <h1 className='text-[1.2rem] mb-4 pb-2 border-b-2'>
             {nextStep ? 'Second' : 'First'} Step
         </h1>
         { !nextStep && <FormPart_1 /> }
         { nextStep && <FormPart_2 /> }
-        <Button type='submit'>
-            {nextStep ? 'Next' : 'Submit'}
-        </Button>
+
+        <div className='border-t-2 pt-3 flex justify-between'>
+          {
+            nextStep &&  <Button className='bg-gray-500 mb-3' onClick={() => setNextStep(false)}>
+                            Go Back
+                        </Button>
+          }
+          <Button type='submit'>
+              {nextStep ? 'Submit' : 'Next'}
+          </Button>
+        </div>
     </form>
   )
 }
--- components/ui/button.js
+++ components/ui/button.js
@@ -2,7 +2,7 @@ export default function Button(props) {
   return (
     <button 
         type={props.type} 
-        className='bg-blue-500 px-6 rounded-md py-2 text-white'
+        className={`bg-blue-500 px-6 rounded-md py-2 text-white ${props.className}`}
         onClick={props.onClick}
     >
         {props.children}
